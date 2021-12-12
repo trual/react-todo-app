a58dfa025c317ef185425c2e188740b6e6ef9912
Alex Trujillo <trual4@gmail.com>
2021-12-12 11:34:52 -0700

    complete filter

--- todo-app/src/components/TodoApp.js
+++ todo-app/src/components/TodoApp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Todo from "./Todo";
 
 
@@ -15,8 +15,10 @@ function TodoApp () {
     }
 
     const [listOfTodos, setListOfTodos] = useState([initialTodo])
+    const [displayListOfTodos, setDisplayListOfTodos] = useState(listOfTodos)
     const [newTodo, setNewTodo] = useState(initialNewTodo)
     const [idCount , setIdCount] = useState(0)
+    const [completedFilter, setCompletedFilter] = useState(false)
 
     function handleInputChange(e) {
         setNewTodo({...newTodo, task: e.target.value })
@@ -60,9 +62,25 @@ function TodoApp () {
         setListOfTodos(listToToggle)
     }
 
+    function handleCompletedFilter() {
+        setCompletedFilter(!completedFilter)
+        console.log(completedFilter)
+    }
+
+    useEffect(() => {
+        let tempList = listOfTodos.map((x) => x)
+
+        //filter completed
+        if (completedFilter) {
+            tempList = tempList.filter((todo) => todo.completed === completedFilter )
+        }
+
+        setDisplayListOfTodos(tempList)
+    }, [listOfTodos, completedFilter])
+
     return <div>
         <ul>
-        {listOfTodos.map(todo => (
+        {displayListOfTodos.map(todo => (
           <Todo 
           key={todo.id} 
           todo={todo}
@@ -75,6 +93,8 @@ function TodoApp () {
             <input value={newTodo.task} onChange={handleInputChange}></input>
             <button>Submit</button>
         </form>
+            <h3>Filters</h3>
+        <button className={completedFilter ? "selectedButton" : null} onClick={handleCompletedFilter}>completed</button>
     </div>
 }
 
